36bc56ac676ec171a95583aee1d05f3ff4c7470e
Ali Venell <ali.venell@gmail.com>
2024-04-03 11:34:22 +0300

    fixed wrong text from 2 others

--- tests/example1.spec.ts
+++ tests/example1.spec.ts
@@ -7,5 +7,5 @@ test('test', async ({ page }) => {
   await page.locator('[data-test="username"]').press('Tab');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('#header_container')).toContainText('wrong text');
+  await expect(page.locator('#header_container')).toContainText('Products');
 });
